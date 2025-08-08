c5d7fbcd1511b9d21fb24f844c4978c7272f4ff7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-08-08 06:08:52 +0200

    Update .cpp

--- .cpp
+++ .cpp
@@ -2506,6 +2506,35 @@ int main(){
     else    cout << "NO\n";
 }
 #include <iostream>
+using namespace std;
+
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0)
+            return false;
+    return true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    // Find the next prime after n
+    int nextPrime = n + 1;
+    while (!isPrime(nextPrime))
+        nextPrime++;
+
+    if (nextPrime == m)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
+
+#include <iostream>
 
 using namespace std;
 
